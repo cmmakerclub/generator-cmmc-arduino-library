c9b2d664f89b52c7a261918dbc16aa3a5adb0e51
Nat <nat@cmmc.io>
2017-12-23 14:25:05 +0700

    bumb to v0.0.2

--- generators/app/templates/lib.cpp
+++ generators/app/templates/lib.cpp
@@ -3,10 +3,3 @@
 void <%= className %>::init() {
 
 }
-
-void <%= className %>::debug(cmmc_debug_cb_t cb) {
-  if (cb != NULL) {
-    this->_user_debug_cb = cb;
-  }
-}
-
--- generators/app/templates/lib.h
+++ generators/app/templates/lib.h
@@ -3,36 +3,15 @@
 
 #include <Arduino.h>
 
-
-#ifndef CMMC_NO_ALIAS
-  #define <%= className %> <%= className %>
-#endif
-
-#ifdef ESP8266
-  extern "C" {
-    #include "user_interface.h"
-  }
-  #include "ESP8266WiFi.h"
-  #include <functional>
-#endif
-
-typedef void (*cmmc_debug_cb_t)(const char* message);
-
 class <%= className %>
 {
     public:
       // constructor
-      <%= className %>() {
-        auto blank = [](const char* message) {};
-      }
-
+      <%= className %>() { }
       ~<%= className %>() {}
 
       void init();
-      void debug(cmmc_debug_cb_t);
     private:
-      cmmc_debug_cb_t _user_debug_cb;
-
 };
 
 #endif //<%= className %>_H
